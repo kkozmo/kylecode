249a3c4a8bb02a6dda4c49aa58e4dafba59940aa
Kyle Kozma <kylekozma@gmail.com>
2014-08-21 21:01:11 -0700

    Changes to navigation bar

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root "pages#home"
+  get "about" => "pages#about"
+  get "fun" => "pages#fun" # This is a path that my fun_path will direct text to for pages
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
